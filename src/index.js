eb5b0a0e3b7287bbc7b85b2f9eaa90f0298835b7
killod <il.masukov@gmail.com>
2019-03-24 16:42:36 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+
+  let dividers = [];
+
+  for (let i = 2; i <= base; i++) {
+    let div  = 0;
+    while (base % i == 0) {
+      base /= i;
+      div++;
+      dividers[i] = div;
+    }
+  }
+
+  let min = number;
+
+  dividers.forEach(function(i, pos, dividers) {
+    let num = number;
+    let count = 0;
+    while (num > 0) {
+      num = Math.floor(num / pos);
+      count += num;
+    }
+    if(Math.floor( count / dividers[pos]) < min) min = Math.floor( count / dividers[pos]);
+  });
+  return min;
 }
